045fa1fd7b46739da0a88f6a86256f1164ee9aab
Brandon-29 <brandon.gj29@gmail.com>
2025-07-13 14:44:21 -0600

    Valores dinamicos

--- 03-ReactEssentials/01-starting-project/src/App.jsx
+++ 03-ReactEssentials/01-starting-project/src/App.jsx
@@ -1,15 +1,34 @@
-import {userState} from 'react';
+import {userState, useState} from 'react';
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import {EXAMPLES} from './data.js';
 
 function App() {
-  userState();
-  let tabContent= "Escoge una opcion";
-  function handleSelect(selectBotton){
-        tabContent=selectBoton;
+ 
+  useState();
+  // const [selectedTopic, setSelectedTopic] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState();
+  function handleSelect(selectedBotton){
+        setSelectedTopic(selectedBotton);
+       // console.log(selectedTopic);
     }
+  let tabContent=<p>Please select a topic</p>;
+  if(selectedTopic){
+    tabContent=(
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
+    );
+
+  }
   return (
     <div>
       <Header />
@@ -34,13 +53,12 @@ function App() {
             {/* Se usa diferente a core concept porque solo se maneja un atributo en la funcion*/}
             {/*<TabButton>Components</TabButton>*/}
             <TabButton onSelect={()=>handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={()=>handleSelect('JSX')}>JSX</TabButton>
+            <TabButton onSelect={()=>handleSelect('jsx')}>JSX</TabButton>
             <TabButton onSelect={()=>handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={()=>handleSelect('state')}>State</TabButton>
-
           </menu>
+            {tabContent}
         </section>
-        
       </main>
     </div>
   );
