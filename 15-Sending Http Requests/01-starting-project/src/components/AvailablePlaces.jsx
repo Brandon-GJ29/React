190941cc90ea279f1bd547ff67777e7a3d35d061
Brandon-29 <brandon.gj29@gmail.com>
2025-07-23 18:29:41 -0600

    Fetch de datos y manejo de carga

---
+++ 15-Sending Http Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -0,0 +1,30 @@
+import Places from './Places.jsx';
+import { useState,useEffect } from 'react';
+//const places = localStorage.getItem('places');
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+
+  const [availablePlaces,setAvailablePlaces] = useState([]);
+
+  useEffect(()=> {
+    async function fetchPlaces(){ 
+     setIsFetching(true);
+     const response = await fetch('http://localhost:3000/places');
+     const resData = await response.json();
+     setAvailablePlaces(resData.places);
+     setIsFetching(false);
+    }
+    fetchPlaces();
+  },[]);
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data.."
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
