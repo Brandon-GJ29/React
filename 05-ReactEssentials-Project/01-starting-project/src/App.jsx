7958b4d377fc9748579754ce42c11446365a10b1
Brandon-29 <brandon.gj29@gmail.com>
2025-07-27 16:56:49 -0600

    Proyecto terminado

--- 05-ReactEssentials-Project/01-starting-project/src/App.jsx
+++ 05-ReactEssentials-Project/01-starting-project/src/App.jsx
@@ -1,33 +1,63 @@
-import { use } from "react";
 import UserInput from "./components/UserInput.jsx";
 import UserResult from "./components/userResult.jsx";
 import { useState } from "react";
 
+import {calculateInvestmentResults} from "./util/investment.js"
+
+
 function App() {
-  const [inversionInicial, setInversionInicial] = useState('');
-  
-   function handleInputChange(value) {
-      setInversionInicial(value);
+  const [inputs, setInputs] = useState({
+    inversionInicial: 10000,
+    inversionAnual: 1200,
+    returnExpected: 6,
+    durationYear: 10,
+
+  });
+  const inputInvalid = inputs.durationYear>=1;
+
+   function handleInputChange(name, value) {
+      setInputs(prev=>(
+        {...prev,
+        [name] : value,}
+      ));
     }
+  const annualData = calculateInvestmentResults({
+    initialInvestment: inputs.inversionInicial,
+    annualInvestment: inputs.inversionAnual,
+    expectedReturn: inputs.returnExpected,
+    duration: inputs.durationYear,
+  });
+
+  console.log(annualData);
   return (
     <main>
       <div id="user-input">
         <ol className="input-group">
           <UserInput nameTag='Inversion Inicial'
-            value ={inversionInicial}
-            onInputChange={handleInputChange}
+            value ={inputs.inversionInicial}
+            onInputChange={value => handleInputChange('inversionInicial', value)} 
           />
-          <UserInput nameTag= 'Inversion Anual'/>
-          <UserInput nameTag='Expected Return'/>
-          <UserInput nameTag= 'Duration'/>
+          <UserInput nameTag= 'Inversion Anual'
+             value ={inputs.inversionAnual}
+            onInputChange={value => handleInputChange('inversionAnual', value)} 
+          
+          />
+          <UserInput nameTag= 'Retorno Esperado'
+             value ={inputs.returnExpected}
+            onInputChange={value => handleInputChange('returnExpected', value)} 
+          
+          />
+          <UserInput nameTag= 'Años'
+             value ={inputs.durationYear}
+            onInputChange={value => handleInputChange('durationYear', value)} 
+          
+          />
+      
         </ol>
       </div>
       <div id="result">
-        <UserResult nameColum={"Year"} result={inversionInicial}/>
-        <UserResult nameColum={"Investment value"}/>
-        <UserResult nameColum={"Interest (year)"}/>
-        <UserResult nameColum={"Total Interest"}/>
-        <UserResult nameColum={"Invested Capital"}/>
+        {!inputInvalid && <p>Ingresa un valor valido</p>}
+        {inputInvalid && <UserResult result={annualData}/>}
 
       </div>
 
--- 05-ReactEssentials-Project/01-starting-project/src/components/UserResult.jsx
+++ 05-ReactEssentials-Project/01-starting-project/src/components/UserResult.jsx
@@ -1,16 +1,33 @@
-export default function UserResult({nameColum, result}){ 
+import { formatter } from "../util/investment";
+export default function UserResult({ result}){ 
+    const initialInvestment = result[0].valueEndOfYear - result[0].interest - result[0].annualInvestment;
     return(
         <table id="result-table">
         <thead>
             <tr>
-                <th>{nameColum}</th>
+                <th>Year</th>
+                <th> Investment Value</th>
+                <th> Interest (year)</th>
+                <th>Total interest</th>
+                <th> Invested Capital</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td> {result} </td>
-          
-            </tr>
+            {result.map(yearData => {
+                const totalInterest = yearData.valueEndOfYear- yearData.annualInvestment * yearData.year - initialInvestment;
+                const totalAmountInvested = yearData.valueEndOfYear- totalInterest;
+            return(
+                <tr key= {yearData.year}>
+                    <td>{yearData.year}</td>
+                    <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                    <td>{formatter.format(yearData.interest)}</td>
+                    <td>{formatter.format(totalInterest)}</td>
+                    <td>{formatter.format(totalAmountInvested)}</td>
+                    
+                </tr>
+                );
+            })}
+        
         </tbody>
         </table>
         
